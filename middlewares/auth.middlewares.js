2d16fa623d17b6fb11e90d926af7b16e29558e51
Heildrum <hugomctapia@gmail.com>
2026-04-21 15:45:20 -0400

    Create auth.middlewares.js
    creacion middlewares.js

---
+++ middlewares/auth.middlewares.js
@@ -0,0 +1,27 @@
+function requireMeliAuth(req, res, next) {
+  try {
+    const cuentasML = require("../config/cuentasML");
+
+    const autorizado =
+      Array.isArray(cuentasML) &&
+      cuentasML.some(cuenta => cuenta.token && cuenta.token.length > 0);
+
+    if (autorizado) {
+      return next();
+    }
+
+    return res.status(401).json({
+      ok: false,
+      message: "No hay cuentas de Mercado Libre autorizadas"
+    });
+
+  } catch (error) {
+    console.error("Error en auth.middleware:", error);
+    return res.status(500).json({
+      ok: false,
+      message: "Error verificando autorización Mercado Libre"
+    });
+  }
+}
+
+module.exports = requireMeliAuth;
